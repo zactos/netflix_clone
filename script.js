7ecf808afab9f00ab373e5c77d76de030f61c342
zactos <78249891+zactos@users.noreply.github.com>
2025-06-08 23:01:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// Navbar CSS property in Scroll
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 100) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
+// Profile section redirecting
+const profileIcon = document.querySelector(".profile-icon");
+profileIcon.addEventListener("click", () => {
+  window.location.href = "profile-details.html";
+});
+
+// Movie card
+const movieCard = document.querySelectorAll(".content-card");
+movieCard.forEach((card) => {
+  card.addEventListener("mouseEnter", () => {
+    card.style.transform = "scale(1.05)";
+  });
+
+  card.addEventListener("mouseleave", () => {
+    card.style.transform = "scale(1)";
+  });
+});
